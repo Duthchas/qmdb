650b57a76ed89ca17c6b52e9f4b5175dc96697b2
Nut <54164110+Duthchas@users.noreply.github.com>
2025-09-10 18:55:57 -0400

    Install Rust and cargo-nextest
    Added installation of Rust and cargo-nextest.

--- install-prereqs-ubuntu.sh
+++ install-prereqs-ubuntu.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get install g++ linux-libc-dev libclang-dev unzip libjemalloc-dev make -y
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 cargo install cargo-nextest
