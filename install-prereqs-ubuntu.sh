db2f89c7e04bbec377ad47897c178db1ae353e8d
Nut <54164110+Duthchas@users.noreply.github.com>
2025-09-10 19:07:24 -0400

    Update package installations in install-prereqs-ubuntu.sh
    Replaced installation of build-essential with clang and libc6-dev.

--- install-prereqs-ubuntu.sh
+++ install-prereqs-ubuntu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get install g++ linux-libc-dev libclang-dev unzip libjemalloc-dev make -y
-sudo apt-get update && sudo apt-get install build-essential
+sudo apt-get install clang libc6-dev
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 cargo install cargo-nextest
